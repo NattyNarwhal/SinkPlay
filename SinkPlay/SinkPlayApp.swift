0ad6345d12c06435aa1a5ae611772d2485c8300c
Calvin Buckley <calvin@cmpct.info>
2022-12-27 15:58:45 -0400

    Initial Commit

---
+++ SinkPlay/SinkPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinkPlayApp.swift
+//  SinkPlay
+//
+//  Created by Calvin Buckley on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct SinkPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
