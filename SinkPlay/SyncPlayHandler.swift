5e4256598ec3d525019f9c78a82e95a98075a319
Calvin Buckley <calvin@cmpct.info>
2023-01-14 15:51:42 -0400

    slowly add a bit more (but not enough)

--- SinkPlay/SyncPlayHandler.swift
+++ SinkPlay/SyncPlayHandler.swift
@@ -38,6 +38,11 @@ class SyncPlayHandler : ChannelInboundHandler {
         let readableBytes = buffer.readableBytes
         if let received = buffer.readString(length: readableBytes) {
             print("C<-S: ", received)
+            // XXX: We should switch the order here, so we get Data and convert to String
+            // Or add JSON decode to another pipeline step
+            if let data = received.data(using: .utf8) {
+                self.handleJsonPayload(data: data)
+            }
         }
     }
     
@@ -51,6 +56,88 @@ class SyncPlayHandler : ChannelInboundHandler {
         NSApplication.shared.presentError(error)
         print("Connection Error: ", error)
         ctx.close(promise: nil)
+    }
+    
+    // Protocol handling
+    private func handleMessageHello(hello: [String: Any]) {
+        for (key, value) in hello {
+            switch (key) {
+            case "username":
+                if let newUsername = value as? String {
+                    DispatchQueue.main.async {
+                        self.appState.nick = newUsername
+                    }
+                }
+            case "version":
+                // XXX: Show this to users somewhere (connection props?)
+                print("Version ", value as! String)
+            case "realversion":
+                // XXX: Show this to users somewhere (connection props?)
+                print("Real Version ", value as! String)
+            case "motd":
+                // XXX: Show this to users
+                print("MOTD:", value as! String)
+            case "room":
+                if let newRoom = value as? [String: String] {
+                    let newRoomName = newRoom["name"]
+                    print("Room:", newRoomName)
+                }
+            case "features":
+                // XXX: Show this to users somewhere (connection props?)
+                if let features = value as? [String: Any] {
+                    for (fKey, fValue) in features {
+                        print("Feature", fKey, ":", fValue)
+                    }
+                }
+            default:
+                print("Unknown Hello key ", value)
+            }
+        }
+    }
+    
+    private func handleMessageSet(set: [String: Any]) {
+        for (key, value) in set {
+            switch (key) {
+            default:
+                print("Unknown Set key ", value)
+            }
+        }
+    }
+    
+    private func handleMessageState(state: [String: Any]) {
+        for (key, value) in state {
+            switch (key) {
+            case "ping":
+                print("Ping")
+            case "playstate":
+                print("State")
+            default:
+                print("Unknown State key ", value)
+            }
+        }
+    }
+    
+    private func handleJsonPayload(data: Data) {
+        if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            switch (json.keys.first) {
+            case "Hello":
+                if let hello = json.values.first! as? [String: Any] {
+                    handleMessageHello(hello: hello)
+                }
+            case "State":
+                if let state = json.values.first! as? [String: Any] {
+                    handleMessageState(state: state)
+                }
+            case "Set":
+                if let set = json.values.first! as? [String: Any] {
+                    handleMessageSet(set: set)
+                }
+            case .none:
+                print("Uh-oh, no key in the payload?")
+            default:
+                print("Don't know how to handle", json.keys.first!) // we checked for none earlier
+            }
+        }
     }
 }
 
